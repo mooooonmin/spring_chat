d51d255d62baa13576c4499c425f9cfcf72a6a23
mooooonmin <minhee610@gmail.com>
2025-02-13 16:44:14 +0900

    jpa 활용 service 구성

--- src/main/java/com/example/demo/domain/auth/service/AuthService.java
+++ src/main/java/com/example/demo/domain/auth/service/AuthService.java
@@ -2,16 +2,64 @@ package com.example.demo.domain.auth.service;
 
 import com.example.demo.domain.auth.model.request.CreateUserRequest;
 import com.example.demo.domain.auth.model.response.CreateUserResponse;
+import com.example.demo.domain.repository.UserRepository;
+import com.example.demo.domain.repository.entity.User;
+import com.example.demo.domain.repository.entity.UserCredentials;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+
+    @Transactional(transactionManager = "createUserTransactionManager") // 커스텀한 트랜잭션매니저 사용
     public CreateUserResponse createUser(CreateUserRequest request) {
+
+        Optional<User> user = userRepository.findByName(request.name());
+
+        if (user.isPresent()) {
+            // TODO 에러
+        }
+
+        try {
+
+            User newUser = this.newUser(request.name());
+            UserCredentials newCredentials = this.newUserCredentials(request.password(), newUser);
+            newUser.setCredentials(newCredentials);
+
+            User savedUser = this.userRepository.save(newUser);
+
+            if (savedUser == null) {
+                // TODO 에러처리
+            }
+
+        } catch (Exception e) {
+
+            // TODO 에러
+
+        }
+
         return new CreateUserResponse(request.name());
+    }
+
+    private User newUser(String name) {
+        User newUser = User.builder()
+                .name(name)
+                .created_at(new Timestamp(System.currentTimeMillis()))
+                .build();
+
+        return newUser;
+    }
+
+    private UserCredentials newUserCredentials(String password, User user) {
+        // TODO hash
     }
 }
--- src/main/java/com/example/demo/domain/repository/UserRepository.java
+++ src/main/java/com/example/demo/domain/repository/UserRepository.java
@@ -1,4 +1,15 @@
 package com.example.demo.domain.repository;
 
-public class UserRepository {
+import com.example.demo.domain.repository.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String name);
+    boolean existsByName(String name);
+
 }
