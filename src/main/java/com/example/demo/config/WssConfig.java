b4e74e902264d1d403f9d21c5366966eba648f0a
mooooonmin <minhee610@gmail.com>
2025-02-14 10:29:56 +0900

    protocol 적용하는 websocket

--- src/main/java/com/example/demo/config/WssConfig.java
+++ src/main/java/com/example/demo/config/WssConfig.java
@@ -3,19 +3,30 @@ package com.example.demo.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
-public class WssConfig implements WebSocketConfigurer {
+public class WssConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*");
+                //.withSockJS();
+    }
+
+/*    @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(null, "/ws/v1/chat")
                 .setAllowedOrigins("*");
-    }
+    }*/
 
 }
