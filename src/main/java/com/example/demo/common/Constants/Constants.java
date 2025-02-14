c1ed461fda47e835303c17fd902a93988b7e419a
mooooonmin <minhee610@gmail.com>
2025-02-14 09:09:46 +0900

    jwt token payload 값 주입

---
+++ src/main/java/com/example/demo/common/Constants/Constants.java
@@ -0,0 +1,11 @@
+package com.example.demo.common.Constants;
+
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Constants {
+
+    public static final long ON_MINUTE_TO_MILLIS = 60 * 1000L;
+
+}
--- src/main/java/com/example/demo/security/JWTProvider.java
+++ src/main/java/com/example/demo/security/JWTProvider.java
@@ -1,8 +1,13 @@
 package com.example.demo.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.example.demo.common.Constants.Constants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.sql.Date;
 
 @Slf4j
 @Component
@@ -31,6 +36,22 @@ public class JWTProvider {
     @Value("${token.refresh-token-time}")
     public void setRefreshTokenTimeForMinute(long refreshTokenTimeForMinute) {
         JWTProvider.refreshTokenTimeForMinute = refreshTokenTimeForMinute;
+    }
+
+    public static String createToken(String name) {
+        return JWT.create()
+                .withSubject(name)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + tokenTimeForMinute * Constants.ON_MINUTE_TO_MILLIS))
+                .sign(Algorithm.HMAC256(secretKey));
+    }
+
+    public static String createRefreshToken(String name) {
+        return JWT.create()
+                .withSubject(name)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + tokenTimeForMinute * Constants.ON_MINUTE_TO_MILLIS))
+                .sign(Algorithm.HMAC256(refreshSecretKey));
     }
 
 }
