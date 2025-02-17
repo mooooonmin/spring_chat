e07843c54ea9ba807a15fd5e3686f8a67e146ed2
mooooonmin <minhee610@gmail.com>
2025-02-17 09:10:17 +0900

    테이블명 수정

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*") // 모든 경로 허용
+        registry.addMapping("/**") // 모든 경로 허용
                 .allowedOriginPatterns("http://localhost:8080:*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
